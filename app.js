fd5b549f2b7f0eacdc689e701f72f3bd9cf44eec
Book Club <henriquej14806@gmail.com>
2025-03-15 19:04:26 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+function selecionarCheckbox(checkbox) {
+    // Seleciona todos os checkboxes dentro da div container
+    var checkboxes = document.querySelectorAll('.container input[type="checkbox"]');
+
+    // Se o checkbox foi marcado, desmarca todos os outros
+    checkboxes.forEach(function(item) {
+        if (item !== checkbox) {
+            item.checked = false; // Desmarca os outros checkboxes
+        }
+    });
+}
+
