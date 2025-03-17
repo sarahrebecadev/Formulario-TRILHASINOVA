4e6cc8a56ee09e1f1498a003bacca84291184bc4
Book Club <henriquej14806@gmail.com>
2025-03-16 22:31:53 -0300

    adicionei mais coisas

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+// validação de cpf 
+let cpf = document.querySelector("#cpf");
+
+cpf.addEventListener("blur", function(){
+   if(cpf.value) cpf.value = cpf.value.match(/.{1,3}/g).join(".").replace(/\.(?=[^.]*$)/,"-");
+});
+
+
 function selecionarCheckbox(checkbox) {
     // Seleciona todos os checkboxes dentro da div container
     var checkboxes = document.querySelectorAll('.container input[type="checkbox"]');
